fc3f63c4c941b749d604eb626dee7af1a488ebfb
itsSachinPrajapati <sachinprajapati2227@gmail.com>
2026-03-04 20:41:38 +0530

    fix cookie settings for cross-origin authentication

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,6 +10,7 @@ const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  
 });
 
 module.exports = db;
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -70,8 +70,8 @@ exports.login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
