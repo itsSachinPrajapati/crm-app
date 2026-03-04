528b1c05b09fd4a4c945b74a9d70c5cf4a7f2015
itsSachinPrajapati <sachinprajapati2227@gmail.com>
2026-03-05 00:09:57 +0530

    Configured production API URL for Vercel deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://crm-app-6mpo.onrender.com/api",
+  const API = import.meta.env.VITE_API_URL;,
   withCredentials: true,
 });
 
